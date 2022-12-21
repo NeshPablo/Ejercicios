f8a853d163cd6ce94af14d6c37f7022673349252
Nesh <neshitopablo@gmail.com>
2022-12-20 22:39:07 -0600

    mas cambios

--- Python/Desafios/chall_1.py
+++ Python/Desafios/chall_1.py
@@ -1,16 +1,3 @@
-class Persona:
-    def __init__(self, nombre, edad):
-        self.nombre = nombre
-        self.edad = edad
 
-    def cumplir_anios(self):
-        self.edad += 1
-        print(f'feliz cumpleaños #{self.edad} {self.nombre}')
-        
-    
-
-persona1 = Persona("paco", 30)
-persona1.cumplir_anios
-print(persona1)
 
 
--- Python/Learn.py
+++ Python/Learn.py
@@ -51,18 +51,124 @@
 """ Libreria reduce para hacer funciones 
     esto con el fin de aprender a ver como es la programacion funcional
 """
-from functools import reduce
+# from functools import reduce
 
-numeros = (3, 2, 6, 7)
+# numeros = (3, 2, 6, 7)
 
-def suma(x, y):
-    return x + y
+# def suma(x, y):
+#     return x + y
 
-sumar = reduce(suma, numeros)
-print(f'Resultado: {sumar}')
+# sumar = reduce(suma, numeros)
+# print(f'Resultado: {sumar}')
 
-""" 
+""" Patrones de busqueda en python
+    con la libreria re
+    que nos facilita la busqueda de caracteres en nustro programa
 """
+# import re
+#1
+# print(re.search(r'\d\d\d', 'askas566skals'))
+
+#2
+# patron = re.compile('\d\d\d')
+# print(patron.search('jols462saas').group())
+
+#3
+# if(re.search('\Aa[0-9].*(end|fin)$', 'a4 es una marca de fin')):
+#     print('Se encontro el patron')
+
+#4 Esta funcion es con la funcion sub que sustituye el valor por un caracter especificado
+# print(re.sub(r'\d', '*', 'mpang8uera990',2))
+
+#5 modificadores en python nos facilita ya que lo detecta sin importar que sea mayuscula o minuscula el caracter
+# regex = re.compile(r'ab', re.IGNORECASE)
+# print(regex.search('jutnkmilajAbui'))
+
+""" Leer archivos XML en python 
+
+from xml.dom.minidom import parse, Node 
+xmltree = parse('')
+
+for nodo in xmltree.getElementsByTagName('pro'):
+    for nodo_hijo in nodo.childNodes:
+        if nodo_hijo.nodeType == Node.TEXT_NODE:
+            print(nodo_hijo.data)
+"""
+# import xml.sax
+# from xml.etree.cElementTree import parse
+
+# xml_doc = parse('')
+# for ele in xml_doc.findall('pro')
+#     print(ele.text)
+
+""" Abrir datos de tipo json en python
+    Con el fin de aprender a abrir diferentes tipos de archivos
+
+"""
+# import json
+
+# with open('') as file:
+#     data = json.load(file)
+
+# print(data)
+""" Enviar un correo electronico con python el cual es necesario tener una cuenta 
+    este solo es un ejemplo ya que si necesitamos que funcione es necesario agregar una cuenta 
+    y ns pida permisos para poder enviar un mensaje 
+"""
+
+# import smtplib
+
+# from email.mime.text import MIMEText
+# smg = MIMEText('Contenido de correo')
+
+# smg['subject'] = 'Asunto del correo'
+# smg['From'] = 'Desdedonde@sjssk.com'
+# smg['To'] = 'Haciadonde@ajsjs.com'
+ 
+# mailServer = smtplib.SMTP('smt.gmail.com', 587)
+# mailServer.ehlo()
+# mailServer.starttls()
+# mailServer.ehlo()
+# mailServer.login('neshss008222@mail.com', 'xxxzxaaxxxxx')
+# mailServer.sendmail('sasasas@mail.com', 'sjjjjsjsjsjs@mail.com', msf_as_string())
+# mailServer.close
+
+"""Calcular area de un cuadrado con una clase
+    o def
+"""
+# def CalcularPerimetro(lado):
+#     area = lado * lado
+#     return area
+
+# area_c = CalcularPerimetro(5)
+# print(area_c)
+
+""" Recibir varios tipos de datos para poder calcular varios datos 
+    utilizando *args ya que nos permite recibir varios tipos de datos
+    sin tener que poner varios datos en la funcion
+"""
+
+# def calcularPerimetro(*args):
+#     perimetro = 0
+#     for lado in args:
+#         perimetro += lado
+#     return perimetro
+
+# permetro = calcularPerimetro(1,2,3,4)
+# print(permetro)
+
+""" En este ejercicio utilizaremos en parametro **kwargs
+    en donde nos acepta poner datos sin poner el nombre del 
+    parametro y nos lo acepta dando un valor como un diccionario
+"""
+def funcionKwargs(**kwargs):
+    print(kwargs)
+    for llave, valor in kwargs.items():
+        print(f'llave: {llave}, valor: {valor}')
+    print(kwargs['nombre'], kwargs['apellido'])
+
+funcionKwargs(nombre='Nesho', apellido='Pablo')
+
 
 
 
