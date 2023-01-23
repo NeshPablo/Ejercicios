5b037e4a6bca2d7c917b8088ccdf39fb8b71f619
Nesh <neshitopablo@gmail.com>
2023-01-23 11:55:36 -0600

    Enviar mensajes por python

---
+++ Python/Desafios/Mensaje.py
@@ -0,0 +1,16 @@
+# Con el codigo se puede enviar cientos de mensajes 
+# los que se quiera enviar
+
+import pyautogui as pt
+import time
+
+limit = input('Cantidad de m,ensajes a enviar: ')
+message = input('Mensaje')
+i = 0
+time.sleep(3)
+
+while i < int(limit):
+    pt.typewrite(message)
+    pt.press('enter')
+    i += 1
+
